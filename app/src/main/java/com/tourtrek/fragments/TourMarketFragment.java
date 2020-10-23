04a0cfd6d855cd52c76e5b495a245add25f25f0e
bobovi <abmensah@wisc.edu>
2020-10-23 01:11:24 -0500

    edits TourMarketFragment.java

--- app/src/main/java/com/tourtrek/fragments/TourMarketFragment.java
+++ app/src/main/java/com/tourtrek/fragments/TourMarketFragment.java
@@ -177,25 +177,11 @@ public class TourMarketFragment extends Fragment implements SearchView.OnQueryTe
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.bottom_nav_menu, menu);
+        inflater.inflate(R.menu.search_menu, menu);
         MenuItem searchItem = menu.findItem(R.id.action_search);
-
-        MenuItem.OnActionExpandListener onActionExpandListener = new
-                MenuItem.OnActionExpandListener() {
-                    @Override
-                    public boolean onMenuItemActionExpand(MenuItem item) {
-                        return true;
-                    }
-
-                    @Override
-                    public boolean onMenuItemActionCollapse(MenuItem item) {
-                        return true;
-                    }
-                };
-        menu.findItem(R.id.action_search).setOnActionExpandListener(onActionExpandListener);
         SearchView searchView = (SearchView) searchItem.getActionView();
         searchView.setOnQueryTextListener(this);
-        searchView.setQueryHint("Search");
+        searchView.setQueryHint("Search tours here");
 
         super.onCreateOptionsMenu(menu, inflater);
 
