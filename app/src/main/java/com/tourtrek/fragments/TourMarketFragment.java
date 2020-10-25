32bf923d6f487a2249935943142e0b1233985ed6
bobovi <abmensah@wisc.edu>
2020-10-25 17:50:47 -0500

    Edit search_menu.xml

--- app/src/main/java/com/tourtrek/fragments/TourMarketFragment.java
+++ app/src/main/java/com/tourtrek/fragments/TourMarketFragment.java
@@ -1,17 +1,22 @@
 package com.tourtrek.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.view.ActionProvider;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
@@ -178,9 +183,7 @@ public class TourMarketFragment extends Fragment implements SearchView.OnQueryTe
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.top_app_bar, menu);
-        menu.findItem(R.id.search);
-
+        inflater.inflate(R.menu.search_menu, menu);
 
         super.onCreateOptionsMenu(menu, inflater);
 
@@ -194,7 +197,7 @@ public class TourMarketFragment extends Fragment implements SearchView.OnQueryTe
     @Override
     public boolean onQueryTextChange(String newText) {
         tourMarketAdapter.getFilter().filter(newText);
-        return false;
+        return true;
     }
 
     @Override
