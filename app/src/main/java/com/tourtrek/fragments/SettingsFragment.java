71597b8bf7efce03f5285cf5b85898918d980782
digiacomo2 <digiacomo2@wisc.edu>
2020-11-04 16:50:49 -0600

    Created SettingsFragment and so far have implemented that when you click the settings button it opens a new xml

---
+++ app/src/main/java/com/tourtrek/fragments/SettingsFragment.java
@@ -0,0 +1,36 @@
+package com.tourtrek.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.tourtrek.R;
+
+public class SettingsFragment extends Fragment {
+    /**
+     * Default for proper back button usage
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                getParentFragmentManager().popBackStack();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View SettingsView = inflater.inflate(R.layout.fragment_tour, container, false);
+            return SettingsView;
+    }
+}
