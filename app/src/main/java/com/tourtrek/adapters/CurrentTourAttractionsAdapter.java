0ef5604e86c9012529de6258849678079f7b0ce5
lovinganivia <cchen335@wisc.edu>
2020-11-11 11:22:20 -0600

    Finish AddFriend and FriendsList

--- app/src/main/java/com/tourtrek/adapters/CurrentTourAttractionsAdapter.java
+++ app/src/main/java/com/tourtrek/adapters/CurrentTourAttractionsAdapter.java
@@ -67,13 +67,11 @@ public class CurrentTourAttractionsAdapter extends RecyclerView.Adapter<CurrentT
         ((MainActivity) context).findViewById(R.id.tour_attractions_rv).setVisibility(View.VISIBLE);
     }
 
-
     @Override
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
     }
-
-
+    
     @Override
     public int getItemCount() {
         return currentTourAttractionsDataSet.size();
