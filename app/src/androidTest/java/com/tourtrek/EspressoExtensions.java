eac37eb3a1c5f9df943a09d38a3baba46ec0e7fc
Josh Rawlins <joshua237@live.com>
2020-11-10 23:20:54 -0600

    future tour test

--- app/src/androidTest/java/com/tourtrek/EspressoExtensions.java
+++ app/src/androidTest/java/com/tourtrek/EspressoExtensions.java
@@ -1,11 +1,19 @@
 package com.tourtrek;
 
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ListView;
+import android.widget.ScrollView;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.test.espresso.PerformException;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ScrollToAction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.espresso.util.HumanReadables;
 import androidx.test.espresso.util.TreeIterables;
 
@@ -13,8 +21,14 @@ import org.hamcrest.Matcher;
 
 import java.util.concurrent.TimeoutException;
 
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
 
 public class EspressoExtensions {
 
@@ -132,5 +146,51 @@ public class EspressoExtensions {
                         .build();
             }
         };
+    }
+
+    /**
+     * This method enables scrolling through a nested scroll view
+     * @return
+     */
+    public static ViewAction nestedScrollTo(){
+        return new ViewAction() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public Matcher<View> getConstraints() {
+                return allOf(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE), isDescendantOfA(anyOf(
+                        isAssignableFrom(ScrollView.class), isAssignableFrom(HorizontalScrollView.class), isAssignableFrom(NestedScrollView.class))));
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                if (isDisplayingAtLeast(90).matches(view)) {
+                    Log.i(TAG, "View is already displayed. Returning.");
+                    return;
+                }
+                Rect rect = new Rect();
+                view.getDrawingRect(rect);
+                if (!view.requestRectangleOnScreen(rect, true /* immediate */)) {
+                    Log.w(TAG, "Scrolling to view was requested, but none of the parents scrolled.");
+                }
+                uiController.loopMainThreadUntilIdle();
+                if (!isDisplayingAtLeast(90).matches(view)) {
+                    throw new PerformException.Builder()
+                            .withActionDescription(this.getDescription())
+                            .withViewDescription(HumanReadables.describe(view))
+                            .withCause(
+                                    new RuntimeException(
+                                            "Scrolling to view was attempted, but the view is not displayed"))
+                            .build();
+                }
+            }
+
+            @Override
+            public String getDescription() {
+                return "scroll to";
+            }
+
+        };
+
     }
 }
