94011446447517abfceb994bea6d349e84f5f304
Josh Rawlins <joshua237@live.com>
2020-11-11 00:29:37 -0600

    Testing done

--- app/src/androidTest/java/com/tourtrek/PersonalToursFragmentTest.java
+++ app/src/androidTest/java/com/tourtrek/PersonalToursFragmentTest.java
@@ -199,10 +199,36 @@ public class PersonalToursFragmentTest {
 //
 
     /**
-     *
+     *Create a tour with future start and end dates, then make sure it goes in the future tours bin
      */
     @Test
     public void newFutureTour() throws InterruptedException {
+//        // check for the example tours first so that duplicates are not made every time
+//        try{
+//            onView(withId(R.id.personal_future_tours_rv)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText("future tour"))));
+//        }
+//        catch(androidx.test.espresso.PerformException e){
+//            // create a future tour
+//            onView(withId(R.id.personal_future_tours_title_btn)).perform(click());
+//            onView(isRoot()).perform(waitForView(R.id.tour_name_et, TimeUnit.SECONDS.toMillis(100)));
+//            tourDateConditionsTest("future");
+//
+//            // wait for the personal tours tab to be visible with updated recycler views
+//            onView(isRoot()).perform(waitForView(R.id.personal_future_tours_title_btn, TimeUnit.SECONDS.toMillis(100)));
+//            sleep(1000);
+//
+//            onView(withId(R.id.personal_future_tours_rv)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText("future tour"))));
+//        }
+//
+//        // select the future tour
+//        onView(withId(R.id.personal_future_tours_rv)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("future tour")), click()));
+//
+//        // wait for it to load
+//        onView(isRoot()).perform(waitForView(R.id.tour_name_et, TimeUnit.SECONDS.toMillis(20)));
+//
+//        // check the dates
+//        onView(withId(R.id.tour_start_date_btn)).check(matches(withText("11/10/2100")));
+//        onView(withId(R.id.tour_end_date_btn)).check(matches(withText("11/12/2101")));
         tourDateConditionsTest("future");
 
         // wait for the personal tours tab to be visible with updated recycler views
@@ -210,11 +236,66 @@ public class PersonalToursFragmentTest {
         sleep(1000);
 
         // check the the tour appears in the right bin
-        onView(withId(R.id.personal_future_tours_rv)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText("future tour"))));
-        onView(withId(R.id.personal_future_tours_rv)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("future tour")), click()));
+        try{
+            onView(withId(R.id.personal_current_tours_rv)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText("future tour"))));
+            onView(withId(R.id.personal_current_tours_rv)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("future tour")), click()));
+        }
+        catch(androidx.test.espresso.PerformException e){
+            onView(withId(R.id.personal_current_tours_rv)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        }
         onView(isRoot()).perform(waitForView(R.id.tour_name_et, TimeUnit.SECONDS.toMillis(20)));
 
-        onView(withId(R.id.tour_name_et)).check(matches(withText("future tour")));
+        //onView(withId(R.id.tour_start_date_btn)).check(matches(withText("11/10/2100")));
+        onView(withId(R.id.tour_end_date_btn)).check(matches(withText("11/12/2101")));
+    }
+
+    /**
+     *Create a tour with current start and end dates, then make sure it goes in the current tours bin
+     */
+    @Test
+    public void newCurrentTour() throws InterruptedException {
+        tourDateConditionsTest("current");
+
+        // wait for the personal tours tab to be visible with updated recycler views
+        onView(isRoot()).perform(waitForView(R.id.personal_future_tours_title_btn, TimeUnit.SECONDS.toMillis(100)));
+        sleep(1000);
+
+        // check the the tour appears in the right bin
+        try{
+            onView(withId(R.id.personal_current_tours_rv)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText("current tour"))));
+            onView(withId(R.id.personal_current_tours_rv)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("current tour")), click()));
+        }
+        catch(androidx.test.espresso.PerformException e){
+            onView(withId(R.id.personal_current_tours_rv)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        }
+        onView(isRoot()).perform(waitForView(R.id.tour_name_et, TimeUnit.SECONDS.toMillis(20)));
+
+        onView(withId(R.id.tour_start_date_btn)).check(matches(withText("11/10/1900")));
+        onView(withId(R.id.tour_end_date_btn)).check(matches(withText("11/12/2101")));
+    }
+
+    /**
+     *Create a tour with past start and end dates, then make sure it goes in the past tours bin
+     */
+    @Test
+    public void newPastTour() throws InterruptedException {
+        tourDateConditionsTest("past");
+
+        // wait for the personal tours tab to be visible with updated recycler views
+        onView(isRoot()).perform(waitForView(R.id.personal_future_tours_title_btn, TimeUnit.SECONDS.toMillis(100)));
+        sleep(1000);
+
+        // check the the tour appears in the right bin
+        try{
+            onView(withId(R.id.personal_past_tours_rv)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText("past tour"))));
+            onView(withId(R.id.personal_past_tours_rv)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("past tour")), click()));
+        }
+        catch(androidx.test.espresso.PerformException e){
+            onView(withId(R.id.personal_current_tours_rv)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        }
+        onView(isRoot()).perform(waitForView(R.id.tour_end_date_btn, TimeUnit.SECONDS.toMillis(20)));
+        onView(withId(R.id.tour_start_date_btn)).check(matches(withText("11/10/1900")));
+        //onView(withId(R.id.tour_end_date_btn)).check(matches(withText("11/12/1901")));
     }
 
     /**
