b237c99a3873fc891b9a873c9c6d8b98cf60266f
Josh Rawlins <joshua237@live.com>
2020-11-13 17:42:55 -0600

    Mike's updated tests

--- app/src/androidTest/java/com/tourtrek/RegistrationFragmentTest.java
+++ app/src/androidTest/java/com/tourtrek/RegistrationFragmentTest.java
@@ -43,12 +43,14 @@ public class RegistrationFragmentTest {
         try {
             onView(isRoot()).perform(waitForView(R.id.navigation_profile, TimeUnit.SECONDS.toMillis(15)));
             onView(withId(R.id.navigation_profile)).perform(click());
+            onView(isRoot()).perform(waitForView(R.id.profile_logout_btn, TimeUnit.SECONDS.toMillis(15)));
             onView(withId(R.id.profile_logout_btn)).perform(click());
         } catch (Exception NoMatchingViewException) {
             Log.w(TAG, "No user is not logged in, continuing test execution");
         } finally {
             onView(withId(R.id.navigation_tours)).perform(click());
-            onView(withId(R.id.login_register_btn)).perform(click());
+            onView(isRoot()).perform(waitForView(R.id.login_register_btn, TimeUnit.SECONDS.toMillis(15)));
+            onView(withId(R.id.login_register_btn)).perform(scrollTo(), click());
         }
 
     }
@@ -57,122 +59,136 @@ public class RegistrationFragmentTest {
     public void RegisterWithNoUsername() throws InterruptedException {
         onView(withId(R.id.register_username_et)).perform(typeText(""));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText("fakeEmail@fake.com"));
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText("fakeEmail@fake.com"));
+        onView(withId(R.id.register_email_et)).perform(typeText("fakeEmail@fake.com"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password1_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password1_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password2_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password2_et)).perform(scrollTo());
+        onView(withId(R.id.register_password2_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_register_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.register_register_btn)).perform(scrollTo());
+        onView(withId(R.id.register_register_btn)).perform(click());
         onView(isRoot()).perform(waitForView(R.id.register_error_tv, TimeUnit.SECONDS.toMillis(1000)));
         onView(withId(R.id.register_error_tv)).check(matches(withText("Not all fields entered")));
     }
     @Test
     public void RegisterWithNoEmail() {
-        onView(withId(R.id.register_username_et)).perform(scrollTo(), typeText("FakeUsername"));
+        onView(withId(R.id.register_username_et)).perform(typeText("FakeUsername"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText(""));
+        onView(withId(R.id.register_email_et)).perform(typeText(""));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password1_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password1_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password2_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password2_et)).perform(scrollTo());
+        onView(withId(R.id.register_password2_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_register_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.register_register_btn)).perform(scrollTo());
+        onView(withId(R.id.register_register_btn)).perform(click());
         onView(isRoot()).perform(waitForView(R.id.register_error_tv, TimeUnit.SECONDS.toMillis(1000)));
         onView(withId(R.id.register_error_tv)).check(matches(withText("Not all fields entered")));
     }
 
     @Test
     public void RegisterWithWrongEmail1() throws InterruptedException {
-        onView(withId(R.id.register_username_et)).perform(scrollTo(), typeText("FakeUsername"));
+        onView(withId(R.id.register_username_et)).perform(typeText("FakeUsername"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText("test123"));
+        onView(withId(R.id.register_email_et)).perform(typeText("test123"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password1_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password1_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password2_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password2_et)).perform(scrollTo());
+        onView(withId(R.id.register_password2_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_register_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.register_register_btn)).perform(scrollTo());
+        onView(withId(R.id.register_register_btn)).perform(click());
         Thread.sleep(1000);
         onView(withId(R.id.register_error_tv)).check(matches(withText("The email address is badly formatted.")));
     }
 
     @Test
     public void RegisterWithWrongEmail2() throws InterruptedException {
-        onView(withId(R.id.register_username_et)).perform(scrollTo(), typeText("FakeUsername"));
+        onView(withId(R.id.register_username_et)).perform(typeText("FakeUsername"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText("@hello.com"));
+        onView(withId(R.id.register_email_et)).perform(typeText("@hello.com"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password1_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password1_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password2_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password2_et)).perform(scrollTo());
+        onView(withId(R.id.register_password2_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_register_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.register_register_btn)).perform(scrollTo());
+        onView(withId(R.id.register_register_btn)).perform(click());
         Thread.sleep(1000);
         onView(withId(R.id.register_error_tv)).check(matches(withText("The email address is badly formatted.")));
     }
     @Test
     public void RegisterWithNoPassword() throws InterruptedException {
-        onView(withId(R.id.register_username_et)).perform(scrollTo(), typeText("FakeUsername"));
+        onView(withId(R.id.register_username_et)).perform(typeText("FakeUsername"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText("fakeEmail@fake.com"));
+        onView(withId(R.id.register_email_et)).perform(typeText("fakeEmail@fake.com"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password1_et)).perform(scrollTo(), typeText(""));
+        onView(withId(R.id.register_password1_et)).perform(typeText(""));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password2_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password2_et)).perform(scrollTo());
+        onView(withId(R.id.register_password2_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_register_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.register_register_btn)).perform(scrollTo());
+        onView(withId(R.id.register_register_btn)).perform(click());
         Thread.sleep(1000);
         onView(withId(R.id.register_error_tv)).check(matches(withText("Not all fields entered")));
     }
 
     @Test
     public void RegisterWithNoPassword2() throws InterruptedException {
-        onView(withId(R.id.register_username_et)).perform(scrollTo(), typeText("FakeUsername"));
+        onView(withId(R.id.register_username_et)).perform(typeText("FakeUsername"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText("fakeEmail@fake.com"));
+        onView(withId(R.id.register_email_et)).perform(typeText("fakeEmail@fake.com"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password1_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password1_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password2_et)).perform(scrollTo(), typeText(""));
+        onView(withId(R.id.register_password2_et)).perform(scrollTo());
+        onView(withId(R.id.register_password2_et)).perform(typeText(""));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_register_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.register_register_btn)).perform(scrollTo());
+        onView(withId(R.id.register_register_btn)).perform(click());
         Thread.sleep(1000);
         onView(withId(R.id.register_error_tv)).check(matches(withText("Not all fields entered")));
     }
 
     @Test
     public void RegisterWithShortPasswords() throws InterruptedException {
-        onView(withId(R.id.register_username_et)).perform(scrollTo(), typeText("FakeUsername"));
+        onView(withId(R.id.register_username_et)).perform(typeText("FakeUsername"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText("fakeEmail@fake.com"));
+        onView(withId(R.id.register_email_et)).perform(typeText("fakeEmail@fake.com"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password1_et)).perform(scrollTo(), typeText("hey"));
+        onView(withId(R.id.register_password1_et)).perform(typeText("hey"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password2_et)).perform(scrollTo(), typeText("hey"));
+        onView(withId(R.id.register_password2_et)).perform(scrollTo());
+        onView(withId(R.id.register_password2_et)).perform(typeText("hey"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_register_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.register_register_btn)).perform(scrollTo());
+        onView(withId(R.id.register_register_btn)).perform(click());
         Thread.sleep(1000);
+        onView(isRoot()).perform(waitForView(R.id.register_error_tv, TimeUnit.SECONDS.toMillis(15)));
         onView(withId(R.id.register_error_tv)).check(matches(withText("The given password is invalid. [ Password should be at least 6 characters ]")));
     }
 
     @Test
     public void RegisterWithMismatchedPasswords() throws InterruptedException {
-        onView(withId(R.id.register_username_et)).perform(scrollTo(), typeText("FakeUsername"));
+        onView(withId(R.id.register_username_et)).perform(typeText("FakeUsername"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_email_et)).perform(scrollTo(), typeText("fakeEmail@fake.com"));
+        onView(withId(R.id.register_email_et)).perform(typeText("fakeEmail@fake.com"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password1_et)).perform(scrollTo(), typeText("password"));
+        onView(withId(R.id.register_password1_et)).perform(typeText("password"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_password2_et)).perform(scrollTo(), typeText("pasdword"));
+        onView(withId(R.id.register_password2_et)).perform(scrollTo(),typeText("pasdword"));
         Espresso.closeSoftKeyboard();
-        onView(withId(R.id.register_register_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.register_register_btn)).perform(scrollTo(),click());
         Thread.sleep(1000);
         onView(withId(R.id.register_error_tv)).check(matches(withText("Passwords do not match")));
     }
 
-
 }
+
 
 
