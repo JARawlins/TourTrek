536fc5ed9136d25a3aecd9aebd428661a0e62b60
Josh_Rawlins <12569180+JARawlins@users.noreply.github.com>
2020-11-30 20:01:34 -0600

    Feature import tour (#105)
    * Feature import tour (#78)
    
    * Update README.md
    
    * Update FriendFragmentTest.java
    
    * I swapped the "Add Attraction" and "Remove Tour" buttons.
    
    * I started on tour importing.
    
    * The core logic for copying a tour from the market into the current user's list of tours works.
    
    * TODOs
    
    Co-authored-by: John Bednarczyk <johnbednarczykjr@gmail.com>
    
    * I corrected the feature so that users have their own attractions when they import tours.
    
    * I cleaned up the code a bit.
    
    * The pull request to fix null pointer crashes got messed up, so I am just committing directly to this feature branch.
    
    * I finished copying over the fixes.
    
    * I removed a duplicate setupImportTourButton method.
    
    Co-authored-by: John Bednarczyk <johnbednarczykjr@gmail.com>

--- app/src/main/java/com/tourtrek/fragments/TourFragment.java
+++ app/src/main/java/com/tourtrek/fragments/TourFragment.java
@@ -115,7 +115,6 @@ import java.util.UUID;
 import static com.tourtrek.utilities.Firestore.updateUser;
 import static com.tourtrek.utilities.PlacesLocal.checkLocationPermission;
 
-// TODO - map scrolling https://stackoverflow.com/questions/14025859/scrollview-is-catching-touch-event-for-google-map
 public class TourFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "TourFragment";
@@ -548,50 +547,6 @@ public class TourFragment extends Fragment implements AdapterView.OnItemSelected
 
     }
 
-    /**
-     * Upon clicking the "Import Tour" button, a copy of the current tour should be added to the user's
-     * account.
-     * Precondition: The button should only be clicked on a tour in the marketplace. Such a tour already has a UID.
-     * @param tourView
-     */
-    private void setupImportTourButton(View tourView) {
-        tourImportButton.setOnClickListener(u -> {
-            // get the current tour
-            Tour tour = tourViewModel.getSelectedTour();
-
-            // create a new Firestore document
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            DocumentReference newTourDoc = db.collection("Tours").document();
-
-            // set the ID of the tour object to that of the new document
-            // the original tour document in the Firestore will not be touched, so changing the ID should be fine
-            tour.setTourUID(newTourDoc.getId());
-            // set the new tour to private by default to avoid cluttering the tour market with it until
-            // after the user has had a chance to modify it
-            tour.setPubliclyAvailable(false);
-
-            // set the content of the new Firestore document
-            newTourDoc.set(tour).addOnCompleteListener(v -> {
-
-                Log.d(TAG, "The tour was imported.");
-//                Toast.makeText(getContext(), "The tour was imported.", Toast.LENGTH_LONG).show();
-
-            })
-                .addOnFailureListener(v1 -> {
-
-                    Log.d(TAG, "Tour importation failed.");
-//                    Toast.makeText(getContext(), "Tour importation failed.", Toast.LENGTH_LONG).show();
-
-                });
-
-            // add the tour to the user's list of tours
-            MainActivity.user.getTours().add(newTourDoc);
-            updateUser();
-
-            // go back
-            getParentFragmentManager().popBackStack();
-        });
-    }
 
     /**
      * Retrieve all attractions belonging to this user
@@ -687,7 +642,7 @@ public class TourFragment extends Fragment implements AdapterView.OnItemSelected
             startDateButton.setEnabled(true);
             endDateButton.setEnabled(true);
             coverImageView.setClickable(true);
-          
+
             updateTourButton.setVisibility(View.VISIBLE);
             tourImportButton.setVisibility(View.GONE);
             addAttractionButton.setVisibility(View.VISIBLE);
@@ -1210,6 +1165,86 @@ public class TourFragment extends Fragment implements AdapterView.OnItemSelected
         // Remove all items from shared preferences
         editor.clear().apply();
     }
+
+    /**
+     * Upon clicking the "Import Tour" button, a copy of the current tour should be added to the user's
+     * account.
+     * Precondition: The button should only be clicked on a tour in the marketplace. Such a tour already has a UID.
+     * @param tourView
+     */
+    private void setupImportTourButton(View tourView) {
+        tourImportButton.setOnClickListener(u -> {
+            // create a new Firestore document
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference newTourDoc = db.collection("Tours").document();
+
+            // part of updating the user's list of tours
+            MainActivity.user.getTours().add(newTourDoc);
+
+            // get a new tour object with an empty list of attractions and the new tour UID
+            Tour tour = copyTour(tourViewModel.getSelectedTour(), newTourDoc.getId(), db);
+
+            // oldAttraction = attractions in the tour to be imported
+            List<DocumentReference> oldAttractions = tourViewModel.getSelectedTour().getAttractions();
+
+            // iterate through the list of existing attractions and create new ones
+            for (DocumentReference attractionRef : oldAttractions){
+                // Add the new attraction document reference to the list of attractions in the tour
+                // Doing it here is important because of lambda expression limitations
+                DocumentReference newAttractionDoc = db.collection("Attractions").document();
+                tour.getAttractions().add(newAttractionDoc);
+                // query the old attraction reference
+                attractionRef.get().addOnSuccessListener(result -> {
+                    // get the old attraction as an object to pull out its fields
+                    Attraction oldAttraction = result.toObject(Attraction.class);
+                    // create the new attraction
+                    Attraction newAttraction = new Attraction(oldAttraction.getReviews(), oldAttraction.getLocation(), oldAttraction.getLat(), oldAttraction.getLon(), oldAttraction.getCost(),
+                            oldAttraction.getName(), oldAttraction.getDescription(), newAttractionDoc.getId(), oldAttraction.getStartDate(), oldAttraction.getStartTime(),
+                            oldAttraction.getEndDate(), oldAttraction.getEndTime(), oldAttraction.getAddress(), oldAttraction.getCoverImageURI(), oldAttraction.getWeather());
+                    // set the new attraction data in Firestore
+                    newAttractionDoc.set(newAttraction).addOnSuccessListener(result2 -> {
+                        Log.d("TourFragment", "Attraction set w/ tour importing");
+                    });
+                });
+            }
+
+            // set the content of the new Firestore Tour document
+            newTourDoc.set(tour).addOnCompleteListener(v -> {
+                Log.d(TAG, "The tour was imported.");
+
+            })
+                .addOnFailureListener(v1 -> {
+                    Log.d(TAG, "Tour importation failed.");
+                });
+
+            // update the user's list of tours
+            updateUser();
+
+            // go back
+            getParentFragmentManager().popBackStack();
+        });
+    }
+
+    /**
+     * Create a new tour which is no different than the one provided except for the UID and having no attraction references.
+     * @param oldTour
+     * @param newUID
+     * @param db
+     * @return
+     */
+    private Tour copyTour(Tour oldTour, String newUID, FirebaseFirestore db){
+
+        // create a new list of attractions
+        List<DocumentReference> newAttractions = new ArrayList<DocumentReference>();
+
+        // make a new tour with the same contents as the old one except it is not publicly available and it has a new document ID
+        Tour newTour = new Tour(oldTour.getName(), oldTour.getStartDate(), oldTour.getEndDate(), oldTour.getLocation(),
+                                oldTour.getCost(), oldTour.getNotifications(), oldTour.getReviews(), oldTour.getDescription(),
+                                false, newAttractions, oldTour.getCoverImageURI(), newUID);
+
+        return newTour;
+    }
+
 
     private void setupNavigationButton(View tourView){
         navigationButton.setOnClickListener(v -> {
